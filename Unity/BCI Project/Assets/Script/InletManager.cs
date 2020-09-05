57a7a86b166681d3d4c4cbf6b997876b4e0c1924
Cassandra Dumas <cassan.dum@gmail.com>
2020-09-05 20:19:47 +0200

    LSL Management

---
+++ Unity/BCI Project/Assets/Script/InletManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using LSL;
+using Assets.LSL4Unity.Scripts.AbstractInlets;
+namespace Assets.LSL4Unity.Scripts.Examples
+{
+    public class InletManager : MonoBehaviour
+    {
+        void Start()
+        {
+        }
+        public void AStreamIsFound(LSLStreamInfoWrapper stream)
+        {
+            Debug.Log(string.Format("New LSL Stream {0} found for {1}", stream.Name, name));
+            Debug.Log(string.Format("New stream has name {0} and type {1}", stream.Name, stream.Type));
+        }
+        public void AStreamGotLost(LSLStreamInfoWrapper stream)
+        {
+            Debug.Log(string.Format("LSL Stream {0} got lost for {1}", stream.Name, name));
+            Debug.Log(string.Format("Lost stream had name {0} and type { 1}", stream.Name, stream.Type));
+        }
+    }
+}
