fb358a5125861bc6a5db7b7101678dc4dd7addda
Cassandra Dumas <cassan.dum@gmail.com>
2020-09-18 23:19:13 +0200

    Thunderstorm

---
+++ Unity/BCI Project/Assets/Script/thunder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using THOR;
+
+public class thunder : MonoBehaviour
+{
+    calibrationMoyenne calibrationmoyenne;
+
+    THOR_Thunderstorm thor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        calibrationmoyenne = GameObject.Find("Calibration").GetComponent<calibrationMoyenne>();
+        thor = GameObject.Find("THOR_Thunderstorm").GetComponent<THOR_Thunderstorm>();
+
+        thor.probability = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (calibrationmoyenne.phase == 3) { thor.probability = 0f; }
+
+        if (calibrationmoyenne.phase == 2) { thor.probability = 0f; }
+
+        if (calibrationmoyenne.phase == 1) { thor.probability = 0f; ; }
+
+        if (calibrationmoyenne.phase == 0) { thor.probability = 0f; }
+
+        if (calibrationmoyenne.phase == -1) { thor.probability = 0f; }
+
+        if (calibrationmoyenne.phase == -2) { thor.probability = 0.5f; }
+
+        if (calibrationmoyenne.phase == -3) { thor.probability = 1f; }
+    }
+}
